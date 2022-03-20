deddbc80f8da247f79b4382b0a676a6b46566098
kankshini2000 <84704527+kankshini2000@users.noreply.github.com>
2022-03-20 11:09:05 +0530

    Delete Spring core directory

--- Spring core/Q3/Bankaccntserviceq3.java
+++
@@ -1,8 +0,0 @@
-package springcore_example;
-
-public interface Bankaccntserviceq3 {
-	public double withdraw(long accId, double balance);
-    public double deposit(long accId, double balance);
-    public double getBalance(long accId);
-    public boolean fundTransfer(long fromAccount, long toAccount, double amont);
-}
