55f0065d9d52fa1871adc52cca0043b704a9179c
kankshini2000 <84704527+kankshini2000@users.noreply.github.com>
2022-04-05 13:24:24 +0530

    Delete JAVASCRIPT DEMOS/Assign directory

--- JAVASCRIPT DEMOS/Assign/Pg 4-5 assignments/q5/main.js
+++
@@ -1,9 +0,0 @@
-var num=prompt("enter the no:")
-let sum=0;
-
-for(let i=1; i<=num ;i++){
-    if(i%3==0 || i%5==0){
-        sum=sum+i;
-    }
-}
-document.write("the sum of nos is: " +sum)
--- JAVASCRIPT DEMOS/Assign/Pg 4-5 assignments/q7/main.js
+++
@@ -1,8 +0,0 @@
-for(let i=1;i<=12;i++){
-    n=1;
-    while(n<=12){
-        document.write(n * i +" ");
-        n+=1;
-    }
-    document.write("<br>");
-}
