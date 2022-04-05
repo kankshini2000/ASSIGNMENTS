63bdbeb1055bed2a7e394fd8cf7aa34ddbc0cc8e
kankshini2000 <84704527+kankshini2000@users.noreply.github.com>
2022-04-05 13:53:18 +0530

    Delete JAVASCRIPT DEMOS/Assign/FUNCTIONS/Function ass directory

--- JAVASCRIPT DEMOS/Assign/FUNCTIONS/Function ass/q3.js
+++
@@ -1,8 +0,0 @@
-var myFunction=function(a,b){return a*b;}
-	    document.write(" "+myFunction);
-
-        function squareMyNumber(no){
-            return no ** 2
-        }
-        let squared = squareMyNumber(5);
-        document.write(" (5) returns ",squared);
